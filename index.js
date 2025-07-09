d50f23b9118189f9a52b892e1fa08480f4714325
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-07-09 15:35:12 +0300

    update

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-function getName(){
+function getName() {
     const users = [
         {
             name: "Lee",
             age: 25,
-            currentCity: "Los Angeles"
+            currentCity: "Los Angeles",
+            getInfo() {
+                console.log(`Name: ${this.name}, Age: ${this.age}, City: ${this.currentCity}`);
+            }
         },
         {
             name: "Kate",
             age: 21,
-            currentCity: "Milan"
+            currentCity: "Milan",
+            getInfo() {
+                console.log(`Name: ${this.name}, Age: ${this.age}, City: ${this.currentCity}`);
+            }
         }
     ];
     users.forEach(user => {
-        console.log(`Name: ${user.name}, Age: ${user.age}, City: ${user.currentCity}`);
+        user.getInfo();
     });
 }
+
 getName();
